7612e9e1c07d6d68c85f43ffde65d50f33b28030
oliverzhao715 <jingliu0215@gmail.com>
2026-03-23 00:58:06 +0000

    Finished Lesson 1

---
+++ lesson1/classwork/printing.js
@@ -0,0 +1,6 @@
+console.log("Hello, world!");
+console.log("My name is Oliver.");
+console.log("My favorite food is ... I dunno.")
+
+// Error: qutation marks CANNOT be mismatched
+// console.log('this doesnt work")
