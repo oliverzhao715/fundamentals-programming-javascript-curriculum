ee3b5b1a5f4676b7a9cbfd322d30cc49a409fd01
oliverzhao715 <jingliu0215@gmail.com>
2026-03-30 00:59:58 +0000

    Finished lesson 2

--- lesson1/homework/problems.js
+++ lesson1/homework/problems.js
@@ -1,24 +1,39 @@
 // Problem 1
 // Create a variable for your lucky number and print it.
 
+let luckynumber = 67;
+console.log(luckynumber);
+
 
 
 // Problem 2
 // Create a variable for the number of hours you sleep and print it.
 
+let sleeptime = 8.5;
+console.log(sleeptime + " hours");
+
 
 
 // Problem 3
 // Create a variable for your favorite fruit and print it in a full sentence.
-
+let favoritefruit = "mangos";
+console.log("my favorite fruit is " + favoritefruit);
 
 
 // Problem 4
 // Create two variables: one for your city and one for your country.
 // Print them on 2 separate lines.
 
-
+let homecity = "Redmond";
+let homecountry =  "US";
+console.log(homecity);
+console.log(homecountry);
 
 // Problem 5
 // Create 3 variables: your pet's name, its age, and its type.
 // Print them on the same line in a full sentence.
+
+let pet_name = "Bob"; // I do not actually have a pet so I made this one up
+let pet_age = 4;
+let pet_type = "dog";
+console.log("My pet's name is " + pet_name + ". He is " + pet_age + " years old. He is a " + pet_type + ".")
---
+++ lesson2/classwork/input.js
@@ -0,0 +1,19 @@
+const prompt = require("prompt-sync")();
+
+// promt always returns a string
+let name = prompt("What is your name?");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
